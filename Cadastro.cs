684c1ee261960690682105204c5f34604b6a3390
MarcosRibeiroFilho <81778890+MarcosRibeiroFilho@users.noreply.github.com>
2021-04-05 15:55:48 -0300

    Add files via upload

---
+++ Cadastro.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Estacionamento_Simples
+{
+    class Cadastro
+    {
+        // Atributos
+        private string nome;
+        private int id;
+        private string phone;
+        private string placa;
+        private string modelo;
+        private string cor;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public string Placa
+        {
+            get { return placa; }
+            set {placa = value; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+
+        public void cadastrar(int ident)
+        {
+            Console.Clear();
+            Console.WriteLine("\tCADASTRO DE USUARIO");
+            Console.Write("Digite o nome do usuario: ");
+            Nome = Console.ReadLine();
+            Id = ident;
+            Console.Write("\nDigite o telefone do usuario: ");
+            Phone = Console.ReadLine();
+            Console.Write("\nDigite a placa do veiculo : ");
+            Placa = Console.ReadLine();
+            Console.Write("\nDigite o modelo do veiculo : ");
+            Modelo = Console.ReadLine();
+            Console.Write("\nDigite a cor do veiculo : ");
+            Cor = Console.ReadLine();
+
+        }
+
+        public void editaCadastro()
+        {
+
+            Console.Write("\n\nDigite o nome do usuario: ");
+            Nome = Console.ReadLine();
+            Console.Write("Digite o telefone do usuario: ");
+            Phone = Console.ReadLine();
+            Console.Write("Digite a placa do veiculo : ");
+            Placa = Console.ReadLine();
+            Console.Write("Digite o modelo do veiculo : ");
+            Modelo = Console.ReadLine();
+            Console.Write("Digite a cor do veiculo : ");
+            Cor = Console.ReadLine();
+
+        }
+
+
+
+
+    }
+
+    class Vaga
+    {
+        public int[] id2= new int[18] {0,32,0,13,0,14,0,0,0,15,0,0,0,10,0,18,0,0};
+        private string hora;
+
+        public void Entra()
+        {
+            int ncar = ContaVaga();
+            if (ncar < 18)
+            {
+                Console.WriteLine("ENTRADA DE VEICULO");
+                Console.Write("Digite o ID: ");
+                int num = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < 18; i++)
+                {
+                    if (id2[i] == 0)
+                    {
+                        id2[i] = num;
+                        return;
+                    }
+                }
+            }
+            else
+                Console.WriteLine("\tESTACIONAMENTO LOTADO");
+        }
+
+        public void Sai()
+        {
+            int ncar = ContaVaga();
+
+            if (ncar > 0)
+            {
+                Console.WriteLine("SAIDA DE VEICULO");
+                Console.Write("Digite o ID: ");
+                int num = int.Parse(Console.ReadLine());
+                bool encontra = false;
+
+                for (int i = 0; i < 18; i++)
+                {
+                    if (id2[i] == num)
+                    {
+                        id2[i] = 0;
+                        encontra = true;
+                        return;
+                    }
+                }
+
+                if (!encontra)
+                    Console.WriteLine("Veiculo nao encontrado");
+            }
+            else
+                Console.WriteLine("Estacionamento vazio\n");
+
+        }
+
+        public void Verifica()
+        {
+            int ncar = ContaVaga();
+            if(ncar > 0)
+            {
+                Console.WriteLine("Os veiculos estacionados possuem os seguintes IDs:");
+                Console.Write("[ ");
+                foreach (int car in id2)
+                {
+                    if(car!=0)
+                       Console.Write("{0}  ", car);
+                }
+                Console.WriteLine("]");
+            }
+        }
+
+        public int ContaVaga()
+        {
+            int cont = 0;
+
+            for (int i = 0; i < 18; i++)
+            {
+                if (id2[i] != 0)
+                    cont++;
+            }
+
+            return cont;
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("   ====================================================");
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write("   ||");
+                for (int i = 0; i < 10; i++)
+                {
+                    if (id2[i] == 0)
+                        Console.Write("   ||");
+                    else
+                        Console.Write(" X ||");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("   ||                                                ||");
+            Console.WriteLine("   ||                                                ||");
+            Console.WriteLine("   ||                                                ||");
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write("   ||        ||");
+                for (int i = 10; i < 18; i++)
+                {
+                    if (id2[i] == 0)
+                        Console.Write("   ||");
+                    else
+                        Console.Write(" X ||");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("   ||        ||========================================");
+            //Console.WriteLine("\t  Estacionamento possui " + (18 - ContaVaga()) + " vagas restantes.");
+            Console.WriteLine("\n");
+        }
+
+        public void ImprimeConsulta()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("  ==============================================================");
+            Console.Write("  ||");
+            for (int i = 0; i < 10; i++)
+            {
+                if (id2[i] == 0)
+                   Console.Write("    ||");
+                else
+                   Console.Write(" {0:00} ||", id2[i]);
+            }
+            Console.Write("\n");
+            
+            Console.WriteLine("  ||                                                          ||");
+            Console.WriteLine("  ||                                                          ||");
+            Console.WriteLine("  ||                                                          ||");
+            
+            Console.Write("  ||          ||");
+            for (int i = 10; i < 18; i++)
+            {
+                 if (id2[i] == 0)
+                    Console.Write("    ||");
+                 else
+                    Console.Write(" {0:00} ||", id2[i]);
+            }
+            Console.Write("\n");
+            
+            Console.WriteLine("  ||          ||================================================");
+            Console.WriteLine("\t        Estacionamento possui " + (18 - ContaVaga()) + " vagas restantes.");
+            Console.WriteLine("\n");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Estacionamento_Simples
+{
+    class Program
+    {
+        static void Main()
+        {
+            char escolha;
+            List<Cadastro> Usuarios = new List<Cadastro>();
+            Vaga S1 = new Vaga();
+            bool Continua = true;
+            int contaId = 0;
+
+            while(Continua)
+            {
+                Console.Clear();
+                Console.WriteLine("\tBEM VINDO AO SISTEMA DE ESTACIONAMENTO\n");
+                S1.Imprime();
+                Console.WriteLine("\n Escolha uma opcao:");
+                Console.WriteLine(" [E] Entrada de veiculo");
+                Console.WriteLine(" [S] Saida de veiculo");
+                Console.WriteLine(" [V] Verificar vagas");
+                Console.WriteLine(" [C] Cadastro de veiculo");
+                Console.WriteLine(" [K] Consultar veiculo");
+                Console.WriteLine(" [T] Editar Cadastro");
+                Console.WriteLine(" [A] Apagar Cadastro");
+                Console.WriteLine(" [O] Sair");
+
+                Console.Write("\n Digite sua opcao: ");
+                escolha = Console.ReadLine()[0];
+                escolha = char.ToUpper(escolha);
+                switch (escolha)
+                {
+                    case 'E': //Entrada de veiculo
+                        Console.Clear();
+                        S1.Entra();
+                        //S1.Imprime();
+                        break;
+
+                    case 'S': //Saida de veiculo
+                        Console.Clear();
+                        S1.Sai();
+                        //S1.Imprime();  
+                        break;
+
+                    case 'V': //Verificar vaga
+                        Console.Clear();
+                        S1.Verifica();
+                        S1.ImprimeConsulta();
+                        Console.WriteLine("\n\tAperte qualquer tecla para retornar ao menu");
+                        Console.ReadLine();
+                        break;
+
+                    case 'C': //Cadastro de veiculo
+                        Usuarios.Add(new Cadastro());
+                        Usuarios[contaId].cadastrar(contaId+1);
+                        contaId = contaId + 1;
+                        Console.WriteLine(contaId);
+                        Console.Clear();
+                        break;
+
+                    case 'K': //Consultar veiculo
+                        
+                        Console.WriteLine("Pessoas cadastradas: " + Usuarios.Count);
+                        int h = consultar();
+                        imprimeCadastro(Usuarios, h);
+                        Console.WriteLine("\n\tAperte qualquer tecla para retornar ao menu");
+                        Console.ReadLine();
+                        break;
+
+                    case 'A': //Apagar Cadastro
+                        Console.Clear();
+                        Console.WriteLine("\tAPAGAR CADASTRO:");
+                        Console.Write("Digite o ID do usuario (ou zero para sair): ");
+                        int apaga = int.Parse(Console.ReadLine());
+                        int posicao = busca(Usuarios, apaga);
+                        if (posicao!=0)
+                        {
+                            Usuarios.RemoveAt(posicao);
+                            Console.WriteLine("\n\tCADASTRO APAGADO");
+                            Console.WriteLine("\n\tAperte qualquer tecla para retornar ao menu");
+                            Console.ReadLine();
+                        }     
+                        break;
+
+                    case 'T': //Editar Cadastro
+                        Console.Clear();
+                        Console.WriteLine("\tEDITAR CADASTRO");
+                        Console.Write("Digite o ID do usuario: ");
+                        int edita = int.Parse(Console.ReadLine());
+                        imprimeCadastro(Usuarios, edita);
+                        int num = busca(Usuarios, edita);
+                        Usuarios[num].editaCadastro();
+                        break;
+
+                    case 'O': //Sair
+                        Console.Clear();
+                        Console.WriteLine("Sair");
+                        Continua = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcao inválida");
+                        break;
+
+                }
+                
+            }
+
+
+
+
+        }
+
+        public static int consultar()
+        {
+           
+            Console.Clear();
+            Console.WriteLine("\tCONSULTAR DADOS DO USUARIO");
+            Console.Write("\nDigite o ID do veículo a ser consultado: ");
+            int x = int.Parse(Console.ReadLine());
+
+            return x;
+
+        }
+
+
+        public static void imprimeCadastro(List<Cadastro> User, int ident)
+        {
+
+            int n_cadastrados = User.Count;
+            bool idEncontrado = false;
+            int indice = 0;
+
+            for (int i = 0; i < n_cadastrados; i++)
+            {
+                if (User[i].Id == ident)
+                {
+                    idEncontrado = true;
+                    indice = i;
+                }
+            }
+
+            if (idEncontrado)
+            {
+                Console.WriteLine("\n\nDADOS DO USUARIO:");
+                Console.Write("Nome do proprietario: ");
+                Console.WriteLine(User[indice].Nome);
+                Console.Write("Telefone do proprietario: ");
+                Console.WriteLine(User[indice].Phone);
+                Console.Write("Placa do veiculo: ");
+                Console.WriteLine(User[indice].Placa);
+                Console.Write("Modelo do veiculo: ");
+                Console.WriteLine(User[indice].Modelo);
+                Console.Write("Cor do veiculo: ");
+                Console.WriteLine(User[indice].Cor);
+
+
+            }
+            else { Console.WriteLine("\n\tCADASTRO NAO ENCONTRADO"); }
+
+
+
+        }
+
+
+        public static int busca(List<Cadastro> User2, int id_busca)
+        {
+            int posicaoLista = 0;
+            for(int i=0; i<User2.Count; i++)
+            {
+                if (User2[i].Id == id_busca)
+                    posicaoLista = i;
+            }
+
+            return posicaoLista;
+            
+        }
+
+
+    }
+}
